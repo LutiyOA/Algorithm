d2b6c2836f24f0545c85f461fd9bdc30f310271d
Lutiy Oleg <lutiy.oleg@yandex.ru>
2023-01-06 01:33:30 +0700

    HomeTask 4

--- test/testTree.java
+++ test/testTree.java
@@ -1,12 +1,13 @@
 package test;
+
 import tree.RedBlackTree;
 
 public class testTree {
     public static void main(String[] args) {
-        RedBlackTree tree=new RedBlackTree();
+        RedBlackTree tree = new RedBlackTree();
         tree.add(9);
         tree.add(1);
-        tree.add(10,9,8,1,0,4,8,6,5,2,7);
+        tree.add(10, 9, 8, 1, 0, 4, 8, 6, 5, 2, 7);
 
     }
 }
